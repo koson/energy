ce7fb470cbace45a8a919cb18d41cee755be9051
TrystanLea <glyn.hudson@openenergymonitor.org>
2016-07-12 16:37:14 +0100

    show write apikey on home screen

--- view/home.php
+++ view/home.php
@@ -2,10 +2,12 @@
 
 <div id="welcome-block">
   <h2>Welcome</h2>
-  <div id="emailout"></div><br>
+  <b>User:</b> <span id="emailout"></span><br><br>
+  <b>Apikey:</b> <span id="apikey_write"></span><br>
 </div>
 
 <script>
-    $("#emailout").html(session.email);
+    $("#emailout").html(session.username);
+    $("#apikey_write").html(session.apikey_write);
     $(".sidenav").show();
 </script>
